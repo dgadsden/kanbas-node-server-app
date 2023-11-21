5d20e45690a052747cb99d18fd566e52217e464a
dgadsden <99849542+dgadsden@users.noreply.github.com>
2023-11-21 12:52:15 -0500

    app.js edits

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ import Hello from "./hello.js";
 
 
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
