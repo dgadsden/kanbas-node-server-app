ab8693498e7139546977ab240ac6914b2b6de8ca
dgadsden <99849542+dgadsden@users.noreply.github.com>
2023-11-21 00:33:06 -0500

    app.js edits

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
@@ -10,7 +11,10 @@ import "dotenv/config";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 app.listen(process.env.PORT || 4000);
+
+
