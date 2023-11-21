ac5b1cc2108b7d6555b3985f3b48d5db5a571366
dgadsden <99849542+dgadsden@users.noreply.github.com>
2023-11-21 00:16:40 -0500

    package.json edits

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 const Hello = (app) => {
-    app.get('/hello', (req, res) => {
+    app.get('./hello', (req, res) => {
         res.send('Life is good!')
     })
-    app.get('/', (req, res) => {
+    app.get('./', (req, res) => {
         res.send('Welcome to Full Stack Development!')
     })
 }
